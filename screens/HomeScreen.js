821d3408684659c77d668b909f89189d51f02442
Fredrik Persson <fredrik.persson@pol.se>
2022-11-02 08:17:33 +0100

    uppstart av deliveroo

---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text className="text-red-700">HomeScreen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
