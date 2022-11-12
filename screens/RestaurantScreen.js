1a03cbdd16cb64ec54ccdeb03bbe21c9f7f40156
Fredrik Persson <fp@pol.se>
2022-11-12 23:57:49 +0100

    dishes

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -9,6 +9,7 @@ import {
   StarIcon,
 } from "react-native-heroicons/solid";
 import { QuestionMarkCircleIcon } from "react-native-heroicons/outline";
+import DishRow from "../components/DishRow";
 
 const Restaurant = () => {
   const navigation = useNavigation();
@@ -78,6 +79,16 @@ const Restaurant = () => {
       <View>
         <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
         {/* Dishrows */}
+        {dishes.map((dish) => (
+          <DishRow
+            key={dish._id}
+            id={dish._id}
+            name={dish.name}
+            description={dish.short_description}
+            price={dish.price}
+            image={dish.image}
+          />
+        ))}
       </View>
     </ScrollView>
   );
